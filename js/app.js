2a75ba380c60c437b3d8e5bdc7a436de29461715
Izabela_Kazior <69646563+IzabelaKaz@users.noreply.github.com>
2020-09-15 08:22:09 -0400

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,18 @@
+function searchBar() {
+    let input = document.getElementById("search");
+  
+    // User input uppercased for case insensitive search
+    const filter = search.value.toUpperCase();
+    const anchors = document.getElementsByClassName("anchor");
+  
+    // Search for filter in each anchor tag
+    for (let anchor of anchors) {
+      const upperCaseCaption = anchor.getAttribute('data-caption').toUpperCase()
+      // Find matching search filter
+      if (upperCaseCaption.includes(filter)) {
+        anchor.classList.remove("non-matched-image")
+      } else {
+        anchor.classList.add("non-matched-image")
+      }
+    }
+  }
